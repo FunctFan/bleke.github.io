dcc069cd888d853b9e47a6fa13a74b9e2c942e8c
FunctFan <jsjxfxd@163.com>
2021-06-30 04:33:12 -0400

    Site updated: 2021-06-30 04:33:11

--- js/motion_global.js
+++ js/motion_global.js
@@ -297,10 +297,10 @@ $(document).ready(function () {
       integrator.next();
       // 可以不加if直接显示
       // 加if的话，要在主题配置文件里面修改 sidebar: always
-      if (CONFIG.sidebar === 'always') {
+      //if (theme..sidebar === 'always') {
        // 模拟点击一下侧边栏按钮
-         sidebarToggleMotion.clickHandler();
-      }
+       //  sidebarToggleMotion.clickHandler();
+      //}
     }
   };
 
